2c91e5389dabb6859108b19c5ef8c0dec05dc9a4
zatzk <zatzk@outlook.com>
2023-07-23 20:12:55 -0300

    refactor: service and css

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -7,15 +7,59 @@ import { ExchangeService } from 'src/app/services/exchange.service';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent {
+  exchangeRateData: any;
+  selectedFromCurrency!: string;
+  selectedToCurrency!: string;
+  amount!: number;
+  convertedAmount!: number;
 
   constructor(private exchangeService: ExchangeService) {
     this.fetchExchangeRate();
    }
 
+  getCurrencySymbol(selectedCurrencyCode: string): string {
+    const selectedCurrency = this.exchangeRateData.currencyList.find((currency: { code: string; }) => currency.code === selectedCurrencyCode);
+    return selectedCurrency ? selectedCurrency.symbol : '';
+  }
+
+  convert() {
+    if (!this.exchangeRateData || !this.amount) {
+      return;
+    }
+
+    const fromRate = this.getRateForCurrency(this.selectedFromCurrency);
+    const toRate = this.getRateForCurrency(this.selectedToCurrency);
+
+    if (!fromRate || !toRate) {
+      return;
+    }
+
+    this.convertedAmount = (this.amount / fromRate) * toRate;
+  }
+  getRateForCurrency(currencyCode: string): number | undefined {
+    return this.exchangeRateData.currencyList.find((currency: any) => currency.code === currencyCode)?.rate;
+  }
+
   fetchExchangeRate() {
     this.exchangeService.getExchangeRate()
     .subscribe((currencies) => {
-      console.log(currencies);
+      const timestamp = currencies.timestamp;
+
+      const currencyList = Object.keys(currencies.rates).map((key) => {
+        return {
+          code: key,
+          name: currencies.rates[key].name,
+          rate: currencies.rates[key].rate,
+          symbol: currencies.rates[key].symbol,
+          emoji: currencies.rates[key].emoji,
+        };
+      });
+      this.exchangeRateData = {
+        timestamp,
+        currencyList
+      };
+      console.log('Fetched exchange rate:', this.exchangeRateData);
+
     });
   }
 }
